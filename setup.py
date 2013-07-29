e6efa3f7949c632919fb8d29732a8f023f27ab8b
Scott Moser <smoser@ubuntu.com>
2013-07-29 16:29:58 -0400

    add initial packaging
    bzr-revno: 9

---
+++ setup.py
@@ -0,0 +1,25 @@
+VERSION = '0.1.0'
+
+from distutils.core import setup
+from glob import glob
+import os
+
+
+def is_f(p):
+    return os.path.isfile(p)
+
+setup(
+    name="curtin",
+    description='The curtin installer',
+    version=VERSION,
+    author='Scott Moser',
+    author_email='scott.moser@canonical.com',
+    license="AGPL",
+    url='http://launchpad.net/curtin/',
+    packages=['curtin', 'curtin.commands'],
+    scripts=glob('bin/*'),
+    data_files=[
+        ('/usr/share/doc/curtin',
+         [f for f in glob('doc/*') if is_f(f)]),
+    ]
+)
