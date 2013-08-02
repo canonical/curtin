b4c409869f919047d07824d250c0716e6dce73e2
Scott Moser <smoser@ubuntu.com>
2013-08-02 09:25:48 -0400

    commit initial unfunctional block_simple
    bzr-revno: 24

--- curtin/block/__init__.py
+++ curtin/block/__init__.py
@@ -15,4 +15,24 @@
 #   You should have received a copy of the GNU Affero General Public License
 #   along with Curtin.  If not, see <http://www.gnu.org/licenses/>.
 
+import errno
+import os
+import stat
+
+
+def get_dev_name_entry(devname):
+    bname = os.path.basename(devname)
+    return (bname, "/dev/" + bname)
+
+
+def is_valid_device(devname):
+    devent = get_dev_name_entry(devname)[1]
+    try:
+        return stat.S_ISBLK(os.stat(devent).st_mode)
+    except OSError as e:
+        if e.errno != errno.ENOENT:
+            raise
+    return False
+
+
 # vi: ts=4 expandtab syntax=python
--- curtin/commands/block_meta.py
+++ curtin/commands/block_meta.py
@@ -15,19 +15,48 @@
 #   You should have received a copy of the GNU Affero General Public License
 #   along with Curtin.  If not, see <http://www.gnu.org/licenses/>.
 
+import curtin.block
+from curtin.log import LOG
+
 
 def block_meta(args):
-    #    curtin block-wipe --all-unused
-    #    curtin block-meta --devices all raid0
-    #    curtin block-meta --devices disk0 simple
-    print("This is block_meta: %s" % args)
-    raise Exception("block_meta is not implemented")
+    # main entry point for the block-meta command.
+    if args.mode == "simple":
+        meta_simple(args)
+    else:
+        raise NotImplementedError("mode=%s is not implemenbed" % args.mode)
+
+
+def meta_simple(args):
+    devices = args.devices
+    if devices is None:
+        LOG.warn("simple mode, no devices given, guessing")
+        devices = ("vda", "sda")
+
+    if len(devices) > 1:
+        if args.devices is not None:
+            LOG.warn("simple mode but multiple devices given. "
+                     "using first found")
+        available = [f for f in devices
+                     if curtin.block.is_valid_device(f)]
+        target = available[0]
+        LOG.warn("mode is 'simple'. multiple devices given. using '%s' "
+                 "(first available)", target)
+    else:
+        target = devices[0]
+
+    if not curtin.block.is_valid_device(target):
+        raise Exception("target device '%s' is not a valid device" % target)
+
+    (devname, devnode) = curtin.block.get_dev_name_entry(target)
+
+    LOG.info("installing in simple mode to '%s'", devname)
 
 
 CMD_ARGUMENTS = (
     ((('-D', '--devices'),
       {'help': 'which devices to operate on', 'action': 'append',
-       'metavar': 'DEVICE'}),
+       'metavar': 'DEVICE', 'default': None, }),
      ('mode', {'help': 'meta-mode to use', 'choices': ['raid0', 'simple']}),
      )
 )
