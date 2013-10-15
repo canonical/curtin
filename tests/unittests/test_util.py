a47c46320b2e634c31dbb8dd0a2eccadb2b5713f
Scott Moser <smoser@ubuntu.com>
2013-10-15 14:27:17 -0400

    fix bug where 'policy-rc.d' was not being cleaned up
    The policy-rc.d file was not being cleaned up because disable_daemons_in_root
    was not returning True when it actually created the file.
    
    That caused the deletion to not be done.
    
    LP: #1238915
    bzr-revno: 84.2.1

--- tests/unittests/test_util.py
+++ tests/unittests/test_util.py
@@ -1,4 +1,7 @@
 from unittest import TestCase
+import os
+import shutil
+import tempfile
 
 from curtin import util
 
@@ -15,6 +18,37 @@ class TestLogTimer(TestCase):
 
         self.assertIn("msg", data)
         self.assertIn("mymessage", data['msg'])
+
+
+class TestDisableDaemons(TestCase):
+    prcpath = "usr/sbin/policy-rc.d"
+
+    def setUp(self):
+        self.target = tempfile.mkdtemp()
+        self.temp_prc = os.path.join(self.target, self.prcpath)
+
+    def tearDown(self):
+        shutil.rmtree(self.target)
+
+    def test_disable_daemons_in_root_works(self):
+        ret = util.disable_daemons_in_root(self.target)
+        self.assertTrue(ret)
+        self.assertTrue(os.path.exists(self.temp_prc))
+
+        ret = util.undisable_daemons_in_root(self.target)
+
+        # return should have been true (it removed) and file should be gone
+        self.assertTrue(ret)
+        self.assertFalse(os.path.exists(self.temp_prc))
+
+    def test_disable_daemons_with_existing_is_false(self):
+        util.write_file(os.path.join(self.target, self.prcpath), "foo")
+        ret = util.disable_daemons_in_root(self.target)
+
+        # the return should have been false (it did not create)
+        # but the file should still exist
+        self.assertFalse(ret)
+        self.assertTrue(os.path.exists(self.temp_prc))
 
 
 # vi: ts=4 expandtab syntax=python
