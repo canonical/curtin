7b1b035433dee3d129b5b94827738a94d1764f32
Newell Jensen <newell.jensen@canonical.com>
2014-09-10 20:04:37 -0700

    Added exception handling and logging for load_reporter and associated logic.
    LP: #1367486
    bzr-revno: 166.1.1

--- curtin/reporter/__init__.py
+++ curtin/reporter/__init__.py
@@ -24,6 +24,7 @@ from abc import (
     ABCMeta,
     abstractmethod,
     )
+from curtin.log import LOG
 from curtin.util import (
     try_import_module,
     )
@@ -61,17 +62,27 @@ class EmptyReporter(BaseReporter):
         """Empty."""
 
 
+class LoadReporterException(Exception):
+    """Raise exception if desired reporter not loaded."""
+    pass
+
+
 def load_reporter(config):
     """Loads and returns reporter instance stored in config file."""
 
     reporter = config.get('reporter')
     if reporter is None:
+        LOG.info("'reporter' not found in config file.")
         return EmptyReporter()
     name, options = reporter.popitem()
     module = try_import_module('curtin.reporter.%s' % name)
     if module is None:
+        LOG.error(
+            "Module for %s reporter could not load." % name)
         return EmptyReporter()
     try:
         return module.load_factory(options)
-    except Exception:
+    except LoadReporterException:
+        LOG.error(
+            "Failed loading %s reporter with %s" % (name, options))
         return EmptyReporter()
