fc242f0292a5e2cf568b8ca4ef13b96618d26ab2
Wesley Wiedenmeier <wesley.wiedenmeier@canonical.com>
2015-06-26 16:14:59 -0500

    Fixed call to get_path_to_storage_volume
    bzr-revno: 207.1.87

--- curtin/commands/block_meta.py
+++ curtin/commands/block_meta.py
@@ -358,7 +358,8 @@ def mount_handler(info, storage_config):
     if state['fstab']:
         with open(state['fstab'], "a") as fp:
             if volume.get('type') in ["raid", "bcache", "lvm_partition"]:
-                location = get_path_to_storage_volume(volume.get('id'))
+                location = get_path_to_storage_volume(volume.get('id'), \
+                        storage_config)
             elif volume.get('type') in ["partition", "dm_crypt"]:
                 location = "UUID=%s" % block.get_volume_uuid(volume_path)
             else:
