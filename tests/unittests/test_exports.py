616d597883fb9523b1f791be29dca429a015bdba
Scott Moser <smoser@ubuntu.com>
2015-07-09 10:08:32 -0400

    remove unintended test file
    bzr-revno: 222.1.7

--- tests/unittests/test_exports.py
+++
@@ -1,8 +0,0 @@
-from unittest import TestCase
-
-import curtin
-
-
-class TestImport(TestCase):
-    def test_exported_kernel_cmdline_copy_to_install(self):
-        self.assertTrue(getattr(curtin, 'KERNEL_CMDLINE_COPY_TO_INSTALL_SEP'))
