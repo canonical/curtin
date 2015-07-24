5e1c2752671789a7d51da22deece50c973003135
Wesley Wiedenmeier <wesley.wiedenmeier@canonical.com>
2015-07-24 18:45:19 -0500

    New setup and teardown for vmtest. Instead of examining disk after installation, try to boot the installed system, giving up if it takes too long. Cloud-init on the installed system writes the data we need into a image that we then mount and examine. This way we can interact with the installed system and run full commands on it and examine the output easily
    bzr-revno: 207.1.143

--- tests/vmtests/__init__.py
+++ tests/vmtests/__init__.py
@@ -1,7 +1,7 @@
-from curtin import util
-
 import os
 import tempfile
+import shutil
+import subprocess
 
 
 class VMBaseClass():
@@ -14,38 +14,82 @@ class VMBaseClass():
         # create tmpdir
         self.tmpdir = tempfile.mkdtemp()
 
-        # create disks
-        for diskno in range(self.number_of_disks):
-            disk_file_name = os.path.join(self.tmpdir, "disk%s.img" % diskno)
-            util.subp(["qemu-img", "create", "-f", "raw", disk_file_name,
-                      "4G"])
-            self.disks.append(disk_file_name)
+        # create target disk
+        target_disk = os.path.join(self.tmpdir, "install_disk.img")
+        subprocess.call(["qemu-img", "create", "-f", "qcow2", target_disk,
+                        "4G"])
 
         # create launch cmd
-        cmd = ["tools/launch", "--netdev=user", "--power=off"]
-        for disk in self.disks:
-            cmd.extend(["-d", disk])
-        cmd.extend([self.boot_img, "--", "curtin", "install"])
-        if self.conf_file:
-            if not os.path.exists(self.conf_file):
-                raise ValueError("specified conf file '%s' does not exist" %
-                                 self.conffile)
-            cmd.extend(["--config=%s" % self.conf_file])
-        cmd.append("cp:///")
+        cmd = ["tools/launch"]
+        if not self.interactive:
+            cmd.append("--silent")
+        cmd.extend(["--power=off", "--netdev=user", "-d", target_disk,
+                   self.boot_img, "--", "curtin", "install", "--config=%s" %
+                   self.conf_file, "cp:///"])
 
-        # run vm
-        util.subp(cmd)
+        # run vm with installer
+        subprocess.call(cmd)
+
+        # write cloud-init for installed system
+        meta_data_file = os.path.join(self.tmpdir, "meta-data")
+        with open(meta_data_file, "w") as fp:
+            fp.write("instance-id: inst-123\n")
+        user_data_file = os.path.join(self.tmpdir, "user-data")
+        with open(user_data_file, "w") as fp:
+            fp.write(self.user_data)
+
+        # create seed.img for installed system's cloud init
+        seed_path = os.path.join(self.tmpdir, "seed.img")
+        subprocess.call(["cloud-localds", seed_path, user_data_file,
+                        meta_data_file])
+
+        # create output disk
+        output_disk = os.path.join(self.tmpdir, "output_disk.img")
+        subprocess.call(["qemu-img", "create", "-f", "raw", output_disk,
+                        "10M"])
+        subprocess.call(["mkfs.ext2", output_disk])
+
+        # create xkvm cmd
+        cmd = ["tools/xkvm", "--netdev=user", "-d", target_disk, "-d",
+               output_disk, "--",
+               "-drive", "file=%s,if=virtio,media=cdrom" % seed_path]
+        if not self.interactive:
+            cmd.extend(["-nographic", "-serial", "file:%s" %
+                       os.path.join(self.tmpdir, "serial.log")])
+
+        # run vm with installed system, fail if timeout expires
+        try:
+            subprocess.call(cmd, timeout=self.timeout)
+        except subprocess.TimeoutExpired:
+            self.timed_out = True
+        else:
+            self.timed_out = False
+
+        # mount output disk
+        self.mnt = os.path.join(self.tmpdir, "mnt")
+        os.mkdir(self.mnt)
+        subprocess.call(["fuseext2", "-o", "rw+", output_disk, self.mnt])
 
     @classmethod
     def tearDownClass(self):
-        # remove disks
-        for disk in self.disks:
-            os.remove(disk)
+        # unmount output disk
+        subprocess.call(["fusermount", "-u", self.mnt])
 
         # remove tempdir
-        os.rmdir(self.tmpdir)
+        shutil.rmtree(self.tmpdir)
 
         # remove logfile
-        os.remove("./serial.log")
+        if os.path.exists("./serial.log"):
+            os.remove("./serial.log")
 
-# vi: ts=4 expandtab syntax=python
+    def source(self, data):
+        ret = {}
+        for line in data.splitlines():
+            if line == "":
+                continue
+            val = line.split('=')
+            ret[val[0]] = val[1]
+        return ret
+
+    def test_did_not_time_out(self):
+        self.assertFalse(self.timed_out)
--- tests/vmtests/test_partitioning.py
+++ tests/vmtests/test_partitioning.py
@@ -1,17 +1,31 @@
 from . import VMBaseClass
-from curtin import util
 from unittest import TestCase
+
+import os
+import textwrap
 
 
 class TestPartitioning(VMBaseClass, TestCase):
     boot_img = "./wily.img"
-    conf_file = "examples/custom-partitioning-test.yaml"
-    number_of_disks = 1
-    disks = []
+    conf_file = "examples/tests/basic.yaml"
+    interactive = False
+    timeout = 300  # If we aren't done in this amount of time give up
+    user_data = textwrap.dedent("""\
+        #cloud-config
+        password: passw0rd
+        chpasswd: { expire: False }
+        bootcmd:
+          - mkdir /media/output
+          - mount /dev/vdb /media/output
+        runcmd:
+          - blkid -o export /dev/vda > /media/output/blkid_output_vda
+          - blkid -o export /dev/vda1 > /media/output/blkid_output_vda1
+          - blkid -o export /dev/vda2 > /media/output/blkid_output_vda2
+        power_state:
+          mode: poweroff
+        """)
 
-    def test_part_table(self):
-        (out, _err) = util.subp(["blkid", "-o", "export", self.disks[0]],
-                                capture=True)
-        current_ptable = list(filter(lambda x: "PTTYPE" in x,
-                                     out.splitlines()))[0].split("=")[-1]
-        self.assertEqual(current_ptable, "dos")
+    def test_ptable(self):
+        with open(os.path.join(self.mnt, "blkid_output_vda")) as fp:
+            blkid_info = self.source(fp.read())
+        self.assertEquals(blkid_info["PTTYPE"], "dos")
