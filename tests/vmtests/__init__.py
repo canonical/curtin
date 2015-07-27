861d9ffa2877f3c41428dca5a9530bc6901624cf
Wesley Wiedenmeier <wesley.wiedenmeier@canonical.com>
2015-07-27 11:15:22 -0500

    Got vmtest framework into usable state
       - Any image can be used that can be located using sstream-query
       - Currently, emulation can only be used with same architecture as host, but
         that can be resolved
       - Images can be cached between runs, and are only redownloaded if checksum is
         bad or files are missing
    
    bzr-revno: 207.1.146

--- tests/vmtests/__init__.py
+++ tests/vmtests/__init__.py
@@ -1,34 +1,55 @@
+import ast
+import hashlib
 import os
 import tempfile
 import shutil
 import subprocess
 
+IMAGE_DIR = "/srv/images/code/curtin-vm-test"
 
-class VMBaseClass():
-    @classmethod
-    def setUpClass(self):
-        # make sure boot img is present
-        if not os.path.exists(self.boot_img):
-            raise ValueError("boot image '%s' does not exist" % self.boot_img)
 
-        # create tmpdir
+class ImageStore:
+    def __init__(self, base_dir):
+        self.base_dir = base_dir
+        if not os.path.isdir(self.base_dir):
+            os.makedirs(self.base_dir)
+
+    def get_image(self, repo, release, arch):
+        # query sstream for root image
+        out = subprocess.check_output(
+            ["tools/usquery", "--max=1", repo, "release=%s" % release,
+             "arch=%s" % arch, "item_name=root-image.gz"])
+        sstream_data = ast.literal_eval(bytes.decode(out))
+
+        # Check if we already have the image
+        checksum = ""
+        release_dir = os.path.join(self.base_dir, repo, release, arch,
+                                   sstream_data['version_name'])
+        p = lambda x: os.path.join(release_dir, x)
+        if os.path.isdir(release_dir) and os.path.exists(p("root-image.gz")):
+            # get checksum of cached image
+            with open(p("root-image.gz"), "rb") as fp:
+                checksum = hashlib.sha256(fp.read()).hexdigest()
+        if not os.path.exists(p("root-image.gz")) or \
+                checksum != sstream_data['sha256'] or \
+                not os.path.exists(p("root-image")) or \
+                not os.path.exists(p("root-image-kernel")) or \
+                not os.path.exists(p("root-image-initrd")):
+            # clear dir, download, and extract image
+            shutil.rmtree(release_dir, ignore_errors=True)
+            os.makedirs(release_dir)
+            subprocess.check_call(
+                ["wget", "-c", sstream_data['item_url'], "-O",
+                 p("root-image.gz")])
+            subprocess.check_call(["tools/maas2roottar", p("root-image.gz")])
+        return (p("root-image"), p("root-image-kernel"),
+                p("root-image-initrd"))
+
+
+class TempDir:
+    def __init__(self, user_data):
+        # Create tmpdir
         self.tmpdir = tempfile.mkdtemp()
-
-        # create target disk
-        target_disk = os.path.join(self.tmpdir, "install_disk.img")
-        subprocess.call(["qemu-img", "create", "-f", "qcow2", target_disk,
-                        "4G"])
-
-        # create launch cmd
-        cmd = ["tools/launch"]
-        if not self.interactive:
-            cmd.append("--silent")
-        cmd.extend(["--power=off", "--netdev=user", "-d", target_disk,
-                   self.boot_img, "--", "curtin", "install", "--config=%s" %
-                   self.conf_file, "cp:///"])
-
-        # run vm with installer
-        subprocess.call(cmd)
 
         # write cloud-init for installed system
         meta_data_file = os.path.join(self.tmpdir, "meta-data")
@@ -36,60 +57,88 @@ class VMBaseClass():
             fp.write("instance-id: inst-123\n")
         user_data_file = os.path.join(self.tmpdir, "user-data")
         with open(user_data_file, "w") as fp:
-            fp.write(self.user_data)
+            fp.write(user_data)
 
-        # create seed.img for installed system's cloud init
-        seed_path = os.path.join(self.tmpdir, "seed.img")
-        subprocess.call(["cloud-localds", seed_path, user_data_file,
-                        meta_data_file])
-
-        # create output disk
-        output_disk = os.path.join(self.tmpdir, "output_disk.img")
-        subprocess.call(["qemu-img", "create", "-f", "raw", output_disk,
-                        "10M"])
-        subprocess.call(["mkfs.ext2", output_disk])
-
-        # create xkvm cmd
-        cmd = ["tools/xkvm", "--netdev=user", "-d", target_disk, "-d",
-               output_disk, "--",
-               "-drive", "file=%s,if=virtio,media=cdrom" % seed_path]
-        if not self.interactive:
-            cmd.extend(["-nographic", "-serial", "file:%s" %
-                       os.path.join(self.tmpdir, "serial.log")])
-
-        # run vm with installed system, fail if timeout expires
-        try:
-            subprocess.call(cmd, timeout=self.timeout)
-        except subprocess.TimeoutExpired:
-            self.timed_out = True
-        else:
-            self.timed_out = False
-
-        # mount output disk
+        # make mnt dir
         self.mnt = os.path.join(self.tmpdir, "mnt")
         os.mkdir(self.mnt)
-        subprocess.call(["fuseext2", "-o", "rw+", output_disk, self.mnt])
 
-    @classmethod
-    def tearDownClass(self):
-        # unmount output disk
-        subprocess.call(["fusermount", "-u", self.mnt])
+        # create target disk
+        self.target_disk = os.path.join(self.tmpdir, "install_disk.img")
+        subprocess.check_call(["qemu-img", "create", "-f", "qcow2",
+                               self.target_disk, "10G"])
 
+        # create seed.img for installed system's cloud init
+        self.seed_disk = os.path.join(self.tmpdir, "seed.img")
+        subprocess.check_call(["cloud-localds", self.seed_disk,
+                               user_data_file, meta_data_file])
+
+        # create output disk, mount ro
+        self.output_disk = os.path.join(self.tmpdir, "output_disk.img")
+        subprocess.check_call(["qemu-img", "create", "-f", "raw",
+                               self.output_disk, "10M"])
+        subprocess.check_call(["mkfs.ext2", self.output_disk])
+
+    def mount_output_disk(self):
+        subprocess.check_call(["fuseext2", "-o", "rw+", self.output_disk,
+                               self.mnt])
+
+    def __del__(self):
         # remove tempdir
         shutil.rmtree(self.tmpdir)
 
-        # remove logfile
+
+class VMBaseClass:
+    @classmethod
+    def setUpClass(self):
+        # get boot img
+        image_store = ImageStore(IMAGE_DIR)
+        (boot_img, boot_kernel, boot_initrd) = image_store.get_image(
+            self.repo, self.release, self.arch)
+
+        # set up tempdir
+        self.td = TempDir(self.user_data)
+
+        # create launch cmd
+        cmd = ["tools/launch"]
+        if not self.interactive:
+            cmd.append("--silent")
+        cmd.extend(["--power=off", "--netdev=user", "-d", self.td.target_disk,
+                   boot_img, "--kernel=%s" % boot_kernel, "--initrd=%s" %
+                   boot_initrd, "--", "curtin", "install", "--config=%s" %
+                   self.conf_file, "cp:///"])
+
+        # run vm with installer
+        subprocess.check_call(cmd, timeout=self.install_timeout)
+
+        # create xkvm cmd
+        cmd = ["tools/xkvm", "--netdev=user", "-d", self.td.target_disk, "-d",
+               self.td.output_disk, "--",
+               "-drive", "file=%s,if=virtio,media=cdrom" % self.td.seed_disk,
+               "-m", "1024"]
+        if not self.interactive:
+            cmd.extend(["-nographic", "-serial", "file:%s" %
+                       os.path.join(self.td.tmpdir, "serial.log")])
+
+        # run vm with installed system, fail if timeout expires
+        subprocess.check_call(cmd, timeout=self.boot_timeout)
+
+        # mount output disk
+        self.td.mount_output_disk()
+
+    @classmethod
+    def tearDownClass(self):
+        subprocess.call(["fusermount", "-u", self.td.mnt])
+        # remove launch logfile
         if os.path.exists("./serial.log"):
             os.remove("./serial.log")
 
-    def source(self, data):
-        ret = {}
-        for line in data.splitlines():
-            if line == "":
-                continue
-            val = line.split('=')
-            ret[val[0]] = val[1]
-        return ret
 
-    def test_did_not_time_out(self):
-        self.assertFalse(self.timed_out)
+def source_data(data):
+    ret = {}
+    for line in data.splitlines():
+        if line == "":
+            continue
+        val = line.split('=')
+        ret[val[0]] = val[1]
+    return ret
--- tests/vmtests/test_partitioning.py
+++ tests/vmtests/test_partitioning.py
@@ -1,4 +1,4 @@
-from . import VMBaseClass
+from . import VMBaseClass, source_data
 from unittest import TestCase
 
 import os
@@ -6,10 +6,13 @@ import textwrap
 
 
 class TestPartitioning(VMBaseClass, TestCase):
-    boot_img = "./wily.img"
+    repo = "maas-daily"
+    release = "wily"
+    arch = "amd64"
     conf_file = "examples/tests/basic.yaml"
+    install_timeout = 600
+    boot_timeout = 120
     interactive = False
-    timeout = 300  # If we aren't done in this amount of time give up
     user_data = textwrap.dedent("""\
         #cloud-config
         password: passw0rd
@@ -26,6 +29,6 @@ class TestPartitioning(VMBaseClass, TestCase):
         """)
 
     def test_ptable(self):
-        with open(os.path.join(self.mnt, "blkid_output_vda")) as fp:
-            blkid_info = self.source(fp.read())
+        with open(os.path.join(self.td.mnt, "blkid_output_vda")) as fp:
+            blkid_info = source_data(fp.read())
         self.assertEquals(blkid_info["PTTYPE"], "dos")
