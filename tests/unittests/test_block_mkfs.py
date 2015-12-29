15fe03541df0228ee384c183b88bc15a1d924545
Wesley Wiedenmeier <wesley.wiedenmeier@gmail.com>
2015-12-29 17:32:43 -0600

    Added unit tests for block.mkfs
    bzr-revno: 328.2.7

---
+++ tests/unittests/test_block_mkfs.py
@@ -0,0 +1,74 @@
+from curtin.block import mkfs
+
+from unittest import TestCase
+import mock
+
+
+class TestBlockMkfs(TestCase):
+
+    def _get_config(self, fstype):
+        return {"fstype": fstype, "type": "format", "id": "testfmt",
+                "volume": "null", "label": "format1",
+                "uuid": "fb26cc6c-ae73-11e5-9e38-2fb63f0c3155"}
+
+    def _assert_same_flags(self, call, expected):
+        for flag in expected:
+            if type(flag) == list:
+                flag_name = flag[0]
+                flag_val = flag[1]
+                self.assertIn(flag_name, call)
+                flag_index = call.index(flag_name)
+                self.assertTrue(len(call) > flag_index)
+                self.assertEquals(call[flag_index + 1], flag_val)
+                call.remove(flag_name)
+                call.remove(flag_val)
+            else:
+                self.assertIn(flag, call)
+                call.remove(flag)
+        # Only remaining vals in call should be mkfs.fstype and dev path
+        self.assertEquals(len(call), 2)
+
+    @mock.patch("curtin.block.mkfs.util")
+    def _run_mkfs_with_config(self, config, expected_cmd, expected_flags,
+                              mock_util):
+        mkfs.mkfs_from_config("/dev/null", config)
+        self.assertTrue(mock_util.subp.called)
+        calls = mock_util.subp.call_args_list
+        self.assertEquals(len(calls), 1)
+        call = calls[0][0][0]
+        self.assertEquals(call[0], expected_cmd)
+        self._assert_same_flags(call, expected_flags)
+
+    def test_mkfs_ext(self):
+        conf = self._get_config("ext4")
+        expected_flags = [["-L", "format1"], "-F", "-q",
+                          ["-U", "fb26cc6c-ae73-11e5-9e38-2fb63f0c3155"]]
+        self._run_mkfs_with_config(conf, "mkfs.ext4", expected_flags)
+
+    def test_mkfs_btrfs(self):
+        conf = self._get_config("btrfs")
+        expected_flags = [["-L", "format1"], "-f",
+                          ["-U", "fb26cc6c-ae73-11e5-9e38-2fb63f0c3155"]]
+        self._run_mkfs_with_config(conf, "mkfs.btrfs", expected_flags)
+
+    def test_mkfs_fat(self):
+        conf = self._get_config("fat32")
+        expected_flags = [["-n", "format1"], ["-F", "32"]]
+        self._run_mkfs_with_config(conf, "mkfs.fat", expected_flags)
+
+        conf = self._get_config("fat")
+        conf['fatsize'] = "16"
+        expected_flags = [["-n", "format1"], ["-F", "16"]]
+        self._run_mkfs_with_config(conf, "mkfs.fat", expected_flags)
+
+    def test_mkfs_errors(self):
+        with self.assertRaises(ValueError):
+            conf = self._get_config(None)
+            self._run_mkfs_with_config(conf, "mkfs.ext4", [])
+        with self.assertRaises(ValueError):
+            conf = self._get_config("fakefilesystemtype")
+            self._run_mkfs_with_config(conf, "mkfs.ext3", [])
+        with self.assertRaises(ValueError):
+            conf = self._get_config("ext4")
+            conf['label'] = "thislabelislongerthan16chars"
+            self._run_mkfs_with_config(conf, "mkfs.ext4", [])
