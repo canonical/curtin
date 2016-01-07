874b4fd996df0b3b0e5d89861ebfb7c636e26a33
Wesley Wiedenmeier <wesley.wiedenmeier@gmail.com>
2016-01-07 00:03:01 -0600

    Remove config parsing from curtin.commands.mkfs and add flags to pass in label and uuid
    bzr-revno: 328.2.22

--- curtin/commands/mkfs.py
+++ curtin/commands/mkfs.py
@@ -15,17 +15,10 @@
 #   You should have received a copy of the GNU Affero General Public License
 #   along with Curtin.  If not, see <http://www.gnu.org/licenses/>.
 
-import curtin.config
-from curtin import config
 from curtin import util
 from . import populate_one_subcmd
-from curtin.block.mkfs import mkfs_from_config
 from curtin.block.mkfs import mkfs as run_mkfs
-from curtin.commands.block_meta import get_path_to_storage_volume
 
-from collections import OrderedDict
-
-import os
 import sys
 
 CMD_ARGUMENTS = (
@@ -36,59 +29,28 @@ CMD_ARGUMENTS = (
      (('-f', '--fstype'),
       {'help': 'filesystem type to use. default is ext4',
        'default': 'ext4', 'action': 'store'}),
-     (('-c', '--config'),
-      {'help': 'read configuration from cfg', 'action': 'append',
-       'metavar': 'CONFIG', 'dest': 'cfgopts', 'default': []})
+     (('-l', '--label'),
+      {'help': 'label to use for filesystem', 'action': 'store'}),
+     (('-u', '--uuid'),
+      {'help': 'uuid to use for filesystem', 'action': 'store'}),
+     (('-F', '--force'),
+      {'help': 'continue if minor errors encountered', 'action': 'store_true',
+       'default': False})
      )
 )
 
 
-def format_blockdev(volume_path, fstype, part_id=None, flags=None):
-    if flags is None:
-        flags = []
-    if part_id is not None:
-        flags.append(("label", part_id))
-    run_mkfs(volume_path, fstype, flags)
-
-
-def format_storage_item(info, storage_config):
-    volume = info.get('volume')
-    if not volume:
-        raise ValueError("volume must be specified for partition '%s'" %
-                         info.get('id'))
-
-    # Get path to volume
-    volume_path = get_path_to_storage_volume(volume, storage_config)
-
-    # Call mkfs_from_config
-    mkfs_from_config(volume_path, info)
-
-
 def mkfs(args):
-    state = util.load_command_environment()
-    cfg = config.load_command_config(args, state)
-
-    if args.cfgopts:
-        for filename in args.cfgopts:
-            curtin.config.merge_config_fp(cfg, open(filename))
-
-    if "storage" in cfg:
-        storage_config = OrderedDict((d["id"], d) for (i, d) in
-                                     enumerate(cfg.get("storage")))
-    else:
-        storage_config = {}
+    flags = []
+    if args.label:
+        flags.append(("label", args.label))
+    if args.uuid:
+        flags.append(("uuid", args.uuid))
+    if args.force:
+        flags.append("force")
 
     for device in args.devices:
-        if device in storage_config:
-            # Device is in storage config
-            format_storage_item(storage_config.get(device), storage_config)
-        elif "/dev/" in device and os.path.exists(device):
-            # Device is path to block dev
-            format_blockdev(device, args.fstype)
-        else:
-            # Bad argument
-            raise ValueError("device '%s' is neither an item in storage "
-                             "config nor a block device" % device)
+        run_mkfs(device, args.fstype, flags, strict=(not args.force))
 
     sys.exit(0)
 
