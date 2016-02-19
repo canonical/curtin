e0af715660f9db990fe71e5fd25d0ff51b39b034
Ryan Harper <ryan.harper@canonical.com>
2016-02-19 15:58:14 -0600

    vmtests: update uefi test to validate target is booted in uefi mode
    bzr-revno: 351.1.7

--- tests/vmtests/test_uefi_basic.py
+++ tests/vmtests/test_uefi_basic.py
@@ -14,6 +14,7 @@ class TestBasicAbs(VMBaseClass):
     install_timeout = 600
     boot_timeout = 120
     extra_disks = []
+    uefi = True
     disk_to_check = {'main_disk': 1}
     collect_scripts = [textwrap.dedent("""
         cd OUTPUT_COLLECT_D
@@ -25,16 +26,35 @@ class TestBasicAbs(VMBaseClass):
         cat /etc/fstab > fstab
         mkdir -p /dev/disk/by-dname
         ls /dev/disk/by-dname/ > ls_dname
+        ls /sys/firmware/efi/ > ls_sys_firmware_efi
         """)]
 
     def test_output_files_exist(self):
         self.output_files_exist(
             ["blkid_output_vda", "blkid_output_vda1", "blkid_output_vda2",
-             "fstab", "ls_dname", "ls_uuid",
+             "fstab", "ls_dname", "ls_uuid", "ls_sys_firmware_efi",
              "proc_partitions"])
 
+    def test_sys_firmware_efi(self):
+        sys_efi_expected = [
+            'config_table',
+            'efivars',
+            'fw_platform_size',
+            'fw_vendor',
+            'runtime',
+            'runtime-map',
+            'systab',
+            'vars',
+        ]
+        sys_efi = self.td.collect + "ls_sys_firmware_efi"
+        if (os.path.exists(sys_efi)):
+            with open(sys_efi) as fp:
+                efi_lines = fp.read().strip().split('\n')
+                self.assertEqual(sorted(sys_efi_expected),
+                                 sorted(efi_lines))
 
-class PreciseTestBasic(relbase.precise, TestBasicAbs):
+
+class PreciseUefiTestBasic(relbase.precise, TestBasicAbs):
     __test__ = True
 
     def test_ptable(self):
