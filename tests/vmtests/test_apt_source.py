8efee4b35fef7c4ffc757c00284c188e16cc1454
Christian Ehrhardt <christian.ehrhardt@canonical.com>
2016-05-30 17:55:27 +0000

    initial collection of apt_source vmtest related data
    bzr-revno: 389.7.33

--- tests/vmtests/test_apt_source.py
+++ tests/vmtests/test_apt_source.py
@@ -18,12 +18,20 @@ class TestAptSrcAbs(VMBaseClass):
         cat /etc/fstab > fstab
         ls /dev/disk/by-dname > ls_dname
         find /etc/network/interfaces.d > find_interfacesd
+        apt-key list "F430BBA5" > key1
+        apt-key list "B59D5F15 97A504B7 E2306DCA 0620BBCF 03683F77" > key2
+        apt-key list "B6832E30" > key3
+        cp /etc/apt/sources.list.d/byobu-ppa.list .
+        cp /etc/apt/sources.list.d/my-repo2.list .
+        cp /etc/apt/sources.list.d/my-repo4.list .
+        ls -1 /etc/apt/sources.list.d/* > sourcelists
         """)]
 
     def test_output_files_exist(self):
         "Check if all output files exist"
         self.output_files_exist(
-            ["fstab"])
+            ["fstab", "key1", "key2", "key3", "byobu-ppa.list",
+             "my-repo2.list", "my-repo4.list", "sourcelists"])
 
 
 class XenialTestAptSrc(relbase.xenial, TestAptSrcAbs):
