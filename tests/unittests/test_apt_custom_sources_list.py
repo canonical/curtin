276d36f5d2ef19d0723f98cdcddf302b7500f123
Christian Ehrhardt <christian.ehrhardt@canonical.com>
2016-06-01 09:47:40 +0200

    convert all apt_custom_sources_list tests to fakerelease
    That makes them independent to the execution environment
    
    bzr-revno: 389.7.57

--- tests/unittests/test_apt_custom_sources_list.py
+++ tests/unittests/test_apt_custom_sources_list.py
@@ -35,7 +35,7 @@ apt_custom_sources_list: |
     # FIND_SOMETHING_SPECIAL
 """
 
-# the custom template above converted on mocked fakerelease
+# the custom and builtin templates converted on mocked fakerelease
 EXPECTED_CONVERTED_CONTENT = """
 ## Note, this file is written by curtin at install time. It should not end
 ## up on the installed system itself.
@@ -55,55 +55,55 @@ EXPECTED_BASE_CONTENT = ("""
 #
 # See http://help.ubuntu.com/community/UpgradeNotes for how to upgrade to
 # newer versions of the distribution.
-deb http://archive.ubuntu.com/ubuntu xenial main restricted
-deb-src http://archive.ubuntu.com/ubuntu xenial main restricted
+deb http://archive.ubuntu.com/ubuntu fakerelease main restricted
+deb-src http://archive.ubuntu.com/ubuntu fakerelease main restricted
 
 ## Major bug fix updates produced after the final release of the
 ## distribution.
-deb http://archive.ubuntu.com/ubuntu xenial-updates main restricted
-deb-src http://archive.ubuntu.com/ubuntu xenial-updates main restricted
+deb http://archive.ubuntu.com/ubuntu fakerelease-updates main restricted
+deb-src http://archive.ubuntu.com/ubuntu fakerelease-updates main restricted
 
 ## N.B. software from this repository is ENTIRELY UNSUPPORTED by the Ubuntu
 ## team. Also, please note that software in universe WILL NOT receive any
 ## review or updates from the Ubuntu security team.
-deb http://archive.ubuntu.com/ubuntu xenial universe
-deb-src http://archive.ubuntu.com/ubuntu xenial universe
-deb http://archive.ubuntu.com/ubuntu xenial-updates universe
-deb-src http://archive.ubuntu.com/ubuntu xenial-updates universe
+deb http://archive.ubuntu.com/ubuntu fakerelease universe
+deb-src http://archive.ubuntu.com/ubuntu fakerelease universe
+deb http://archive.ubuntu.com/ubuntu fakerelease-updates universe
+deb-src http://archive.ubuntu.com/ubuntu fakerelease-updates universe
 
 ## N.B. software from this repository is ENTIRELY UNSUPPORTED by the Ubuntu
 ## team, and may not be under a free licence. Please satisfy yourself as to
 ## your rights to use the software. Also, please note that software in
 ## multiverse WILL NOT receive any review or updates from the Ubuntu
 ## security team.
-deb http://archive.ubuntu.com/ubuntu xenial multiverse
-deb-src http://archive.ubuntu.com/ubuntu xenial multiverse
-deb http://archive.ubuntu.com/ubuntu xenial-updates multiverse
-deb-src http://archive.ubuntu.com/ubuntu xenial-updates multiverse
+deb http://archive.ubuntu.com/ubuntu fakerelease multiverse
+deb-src http://archive.ubuntu.com/ubuntu fakerelease multiverse
+deb http://archive.ubuntu.com/ubuntu fakerelease-updates multiverse
+deb-src http://archive.ubuntu.com/ubuntu fakerelease-updates multiverse
 
 ## N.B. software from this repository may not have been tested as
 ## extensively as that contained in the main release, although it includes
 ## newer versions of some applications which may provide useful features.
 ## Also, please note that software in backports WILL NOT receive any review
 ## or updates from the Ubuntu security team.
-deb http://archive.ubuntu.com/ubuntu xenial-backports"""
+deb http://archive.ubuntu.com/ubuntu fakerelease-backports"""
                          """ main restricted universe multiverse
-deb-src http://archive.ubuntu.com/ubuntu xenial-backports"""
+deb-src http://archive.ubuntu.com/ubuntu fakerelease-backports"""
                          """ main restricted universe multiverse
 
-deb http://security.ubuntu.com/ubuntu xenial-security main restricted
-deb-src http://security.ubuntu.com/ubuntu xenial-security main restricted
-deb http://security.ubuntu.com/ubuntu xenial-security universe
-deb-src http://security.ubuntu.com/ubuntu xenial-security universe
-deb http://security.ubuntu.com/ubuntu xenial-security multiverse
-deb-src http://security.ubuntu.com/ubuntu xenial-security multiverse
+deb http://security.ubuntu.com/ubuntu fakerelease-security main restricted
+deb-src http://security.ubuntu.com/ubuntu fakerelease-security main restricted
+deb http://security.ubuntu.com/ubuntu fakerelease-security universe
+deb-src http://security.ubuntu.com/ubuntu fakerelease-security universe
+deb http://security.ubuntu.com/ubuntu fakerelease-security multiverse
+deb-src http://security.ubuntu.com/ubuntu fakerelease-security multiverse
 
 ## Uncomment the following two lines to add software from Canonical's
 ## 'partner' repository.
 ## This software is not part of Ubuntu, but is offered by Canonical and the
 ## respective vendors as a service to Ubuntu users.
-# deb http://archive.canonical.com/ubuntu xenial partner
-# deb-src http://archive.canonical.com/ubuntu xenial partner
+# deb http://archive.canonical.com/ubuntu fakerelease partner
+# deb-src http://archive.canonical.com/ubuntu fakerelease partner
 """)
 
 EXPECTED_MIRROR_CONTENT = ("""
@@ -112,55 +112,55 @@ EXPECTED_MIRROR_CONTENT = ("""
 #
 # See http://help.ubuntu.com/community/UpgradeNotes for how to upgrade to
 # newer versions of the distribution.
-deb http://archive.ubuntu.com/ubuntu xenial main restricted
-deb-src http://archive.ubuntu.com/ubuntu xenial main restricted
+deb http://archive.ubuntu.com/ubuntu fakerelease main restricted
+deb-src http://archive.ubuntu.com/ubuntu fakerelease main restricted
 
 ## Major bug fix updates produced after the final release of the
 ## distribution.
-deb http://archive.ubuntu.com/ubuntu xenial-updates main restricted
-deb-src http://archive.ubuntu.com/ubuntu xenial-updates main restricted
+deb http://archive.ubuntu.com/ubuntu fakerelease-updates main restricted
+deb-src http://archive.ubuntu.com/ubuntu fakerelease-updates main restricted
 
 ## N.B. software from this repository is ENTIRELY UNSUPPORTED by the Ubuntu
 ## team. Also, please note that software in universe WILL NOT receive any
 ## review or updates from the Ubuntu security team.
-deb http://archive.ubuntu.com/ubuntu xenial universe
-deb-src http://archive.ubuntu.com/ubuntu xenial universe
-deb http://archive.ubuntu.com/ubuntu xenial-updates universe
-deb-src http://archive.ubuntu.com/ubuntu xenial-updates universe
+deb http://archive.ubuntu.com/ubuntu fakerelease universe
+deb-src http://archive.ubuntu.com/ubuntu fakerelease universe
+deb http://archive.ubuntu.com/ubuntu fakerelease-updates universe
+deb-src http://archive.ubuntu.com/ubuntu fakerelease-updates universe
 
 ## N.B. software from this repository is ENTIRELY UNSUPPORTED by the Ubuntu
 ## team, and may not be under a free licence. Please satisfy yourself as to
 ## your rights to use the software. Also, please note that software in
 ## multiverse WILL NOT receive any review or updates from the Ubuntu
 ## security team.
-deb http://archive.ubuntu.com/ubuntu xenial multiverse
-deb-src http://archive.ubuntu.com/ubuntu xenial multiverse
-deb http://archive.ubuntu.com/ubuntu xenial-updates multiverse
-deb-src http://archive.ubuntu.com/ubuntu xenial-updates multiverse
+deb http://archive.ubuntu.com/ubuntu fakerelease multiverse
+deb-src http://archive.ubuntu.com/ubuntu fakerelease multiverse
+deb http://archive.ubuntu.com/ubuntu fakerelease-updates multiverse
+deb-src http://archive.ubuntu.com/ubuntu fakerelease-updates multiverse
 
 ## N.B. software from this repository may not have been tested as
 ## extensively as that contained in the main release, although it includes
 ## newer versions of some applications which may provide useful features.
 ## Also, please note that software in backports WILL NOT receive any review
 ## or updates from the Ubuntu security team.
-deb http://archive.ubuntu.com/ubuntu xenial-backports"""
+deb http://archive.ubuntu.com/ubuntu fakerelease-backports"""
                            """ main restricted universe multiverse
-deb-src http://archive.ubuntu.com/ubuntu xenial-backports"""
+deb-src http://archive.ubuntu.com/ubuntu fakerelease-backports"""
                            """ main restricted universe multiverse
 
-deb http://archive.ubuntu.com/ubuntu xenial-security main restricted
-deb-src http://archive.ubuntu.com/ubuntu xenial-security main restricted
-deb http://archive.ubuntu.com/ubuntu xenial-security universe
-deb-src http://archive.ubuntu.com/ubuntu xenial-security universe
-deb http://archive.ubuntu.com/ubuntu xenial-security multiverse
-deb-src http://archive.ubuntu.com/ubuntu xenial-security multiverse
+deb http://archive.ubuntu.com/ubuntu fakerelease-security main restricted
+deb-src http://archive.ubuntu.com/ubuntu fakerelease-security main restricted
+deb http://archive.ubuntu.com/ubuntu fakerelease-security universe
+deb-src http://archive.ubuntu.com/ubuntu fakerelease-security universe
+deb http://archive.ubuntu.com/ubuntu fakerelease-security multiverse
+deb-src http://archive.ubuntu.com/ubuntu fakerelease-security multiverse
 
 ## Uncomment the following two lines to add software from Canonical's
 ## 'partner' repository.
 ## This software is not part of Ubuntu, but is offered by Canonical and the
 ## respective vendors as a service to Ubuntu users.
-# deb http://archive.canonical.com/ubuntu xenial partner
-# deb-src http://archive.canonical.com/ubuntu xenial partner
+# deb http://archive.canonical.com/ubuntu fakerelease partner
+# deb-src http://archive.canonical.com/ubuntu fakerelease partner
 """)
 
 EXPECTED_PRIMSEC_CONTENT = ("""
@@ -169,55 +169,55 @@ EXPECTED_PRIMSEC_CONTENT = ("""
 #
 # See http://help.ubuntu.com/community/UpgradeNotes for how to upgrade to
 # newer versions of the distribution.
-deb http://archive.ubuntu.com/ubuntu xenial main restricted
-deb-src http://archive.ubuntu.com/ubuntu xenial main restricted
+deb http://archive.ubuntu.com/ubuntu fakerelease main restricted
+deb-src http://archive.ubuntu.com/ubuntu fakerelease main restricted
 
 ## Major bug fix updates produced after the final release of the
 ## distribution.
-deb http://archive.ubuntu.com/ubuntu xenial-updates main restricted
-deb-src http://archive.ubuntu.com/ubuntu xenial-updates main restricted
+deb http://archive.ubuntu.com/ubuntu fakerelease-updates main restricted
+deb-src http://archive.ubuntu.com/ubuntu fakerelease-updates main restricted
 
 ## N.B. software from this repository is ENTIRELY UNSUPPORTED by the Ubuntu
 ## team. Also, please note that software in universe WILL NOT receive any
 ## review or updates from the Ubuntu security team.
-deb http://archive.ubuntu.com/ubuntu xenial universe
-deb-src http://archive.ubuntu.com/ubuntu xenial universe
-deb http://archive.ubuntu.com/ubuntu xenial-updates universe
-deb-src http://archive.ubuntu.com/ubuntu xenial-updates universe
+deb http://archive.ubuntu.com/ubuntu fakerelease universe
+deb-src http://archive.ubuntu.com/ubuntu fakerelease universe
+deb http://archive.ubuntu.com/ubuntu fakerelease-updates universe
+deb-src http://archive.ubuntu.com/ubuntu fakerelease-updates universe
 
 ## N.B. software from this repository is ENTIRELY UNSUPPORTED by the Ubuntu
 ## team, and may not be under a free licence. Please satisfy yourself as to
 ## your rights to use the software. Also, please note that software in
 ## multiverse WILL NOT receive any review or updates from the Ubuntu
 ## security team.
-deb http://archive.ubuntu.com/ubuntu xenial multiverse
-deb-src http://archive.ubuntu.com/ubuntu xenial multiverse
-deb http://archive.ubuntu.com/ubuntu xenial-updates multiverse
-deb-src http://archive.ubuntu.com/ubuntu xenial-updates multiverse
+deb http://archive.ubuntu.com/ubuntu fakerelease multiverse
+deb-src http://archive.ubuntu.com/ubuntu fakerelease multiverse
+deb http://archive.ubuntu.com/ubuntu fakerelease-updates multiverse
+deb-src http://archive.ubuntu.com/ubuntu fakerelease-updates multiverse
 
 ## N.B. software from this repository may not have been tested as
 ## extensively as that contained in the main release, although it includes
 ## newer versions of some applications which may provide useful features.
 ## Also, please note that software in backports WILL NOT receive any review
 ## or updates from the Ubuntu security team.
-deb http://archive.ubuntu.com/ubuntu xenial-backports"""
+deb http://archive.ubuntu.com/ubuntu fakerelease-backports"""
                             """ main restricted universe multiverse
-deb-src http://archive.ubuntu.com/ubuntu xenial-backports"""
+deb-src http://archive.ubuntu.com/ubuntu fakerelease-backports"""
                             """ main restricted universe multiverse
 
-deb http://security.ubuntu.com/ubuntu xenial-security main restricted
-deb-src http://security.ubuntu.com/ubuntu xenial-security main restricted
-deb http://security.ubuntu.com/ubuntu xenial-security universe
-deb-src http://security.ubuntu.com/ubuntu xenial-security universe
-deb http://security.ubuntu.com/ubuntu xenial-security multiverse
-deb-src http://security.ubuntu.com/ubuntu xenial-security multiverse
+deb http://security.ubuntu.com/ubuntu fakerelease-security main restricted
+deb-src http://security.ubuntu.com/ubuntu fakerelease-security main restricted
+deb http://security.ubuntu.com/ubuntu fakerelease-security universe
+deb-src http://security.ubuntu.com/ubuntu fakerelease-security universe
+deb http://security.ubuntu.com/ubuntu fakerelease-security multiverse
+deb-src http://security.ubuntu.com/ubuntu fakerelease-security multiverse
 
 ## Uncomment the following two lines to add software from Canonical's
 ## 'partner' repository.
 ## This software is not part of Ubuntu, but is offered by Canonical and the
 ## respective vendors as a service to Ubuntu users.
-# deb http://archive.canonical.com/ubuntu xenial partner
-# deb-src http://archive.canonical.com/ubuntu xenial partner
+# deb http://archive.canonical.com/ubuntu fakerelease partner
+# deb-src http://archive.canonical.com/ubuntu fakerelease partner
 """)
 
 
@@ -243,7 +243,9 @@ class TestAptSourceConfigSourceList(TestCase):
         with mock.patch.object(util, 'write_file') as mockwrite:
             # keep it side effect free and avoid permission errors
             with mock.patch.object(os, 'rename'):
-                apt_source.handle_apt_source(cfg)
+                with mock.patch.object(apt_source, 'get_release',
+                                       return_value='fakerelease'):
+                    apt_source.handle_apt_source(cfg)
 
         mockwrite.assert_called_once_with(
             '/etc/apt/sources.list',
