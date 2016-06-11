73d7f589fafbc259ac61a2484ee08a2fa10e5dbd
Wesley Wiedenmeier <wesley.wiedenmeier@gmail.com>
2016-06-10 20:07:55 -0500

    Wrote out structure for block.clear_holders
      - The function clear_holders() will supress IOErrors that may occur while it
        is running. However, these errors will not be discarded, they will just be
        stored and returned at the end of the function.
      - At the end of clear_holders(), holders will be checked again. If any
        holders remain or any errors are encountered listing holders, clear_holders
        will return False, signaling that installation should not continue.
      - Calls to the function clear_holders() can be made through check_clear(),
        which will check the return value of clear_holders and will write all
        errors encountered by clear_holders to the log, then raise an OSError to
        stop installation if holders could not be shut down.
      - Logic for shutting down bcache volumes, mdadm volumes and lvm volumes has
        been moved into separate functions as it may be useful for other parts of
        the program. This also allows it to be unittested.
    
    bzr-revno: 387.4.8

--- curtin/block/clear_holders.py
+++ curtin/block/clear_holders.py
@@ -25,32 +25,118 @@ from curtin.log import LOG
 import os
 
 
-def get_holders(devname=None, sysfs_path=None):
+def shutdown_bcache(device):
+    pass
+
+
+def shutdown_mdadm(device):
+    pass
+
+
+def shutdown_lvm(device):
+    pass
+
+
+def get_holders(device):
     """
     Look up any block device holders.
     Can handle devices and partitions as devnames (vdb, md0, vdb7)
     Can also handle devices and partitions by path in /sys/
     Will not raise io errors, but will collect and return them
     """
-    if not devname and not sysfs_path:
-        raise ValueError("either devname or sysfs_path must be supplied")
-
     holders = []
     catcher = util.ForgiveIoError()
 
     # get sysfs path if missing
-    if not sysfs_path:
-        with catcher:
-            sysfs_path = block.sys_block_path(devname)
+    # block.sys_block_path works when given a /sys or /dev path
+    with catcher:
+        sysfs_path = block.sys_block_path(device)
 
     # block.sys_block_path may have failed
     if not sysfs_path:
-        LOG.debug('get_holders: did not find sysfs path for %s', devname)
+        LOG.debug('get_holders: did not find sysfs path for %s', device)
         return (holders, catcher.caught)
 
     # get holders
     with catcher:
         holders = os.listdir(os.path.join(sysfs_path, 'holders'))
 
-    LOG.debug("devname '%s' had holders: %s", devname, ','.join(holders))
+    LOG.debug("devname '%s' had holders: %s", device, ','.join(holders))
     return (holders, catcher.caught)
+
+
+def clear_holders(device):
+    """
+    Shutdown all storage layers holding specified device.
+    Device can be specified either with a path in /dev or /sys/block
+
+    Will supress all io errors encountered while removing holders, as there may
+    be situations in which shutting down one holding device may remove another,
+    causing it to disappear. Once all handlers have been run, check if all
+    holders have been shut down.
+
+    Returns True is all holders could be shut down sucessfully, False
+    otherwise. Also returns a list of all IOErrors encountered and ignored
+    while running.
+    """
+    catcher = util.ForgiveIoError()
+
+    # block.sys_block_path works when given a /sys path as well
+    with catcher:
+        device = block.sys_block_path(device)
+
+    (holders, _err) = get_holders(device)
+    catcher.caught.extend(_err)
+    LOG.info("clear_holders running on '%s', with holders '%s'" %
+             (device, holders))
+
+    # if there were no holders or the holders dir could not be accessed, skip
+    if not holders:
+        return (True, catcher.caught)
+
+    # go through all found holders, get their real path, detect what type they
+    # are, and shut them down
+    for holder in holders:
+        # get realpath, skip holder if cannot get it, as holder may be gone
+        # already
+        holder_realpath = None
+        with catcher:
+            holder_realpath = os.path.realpath(os.path.join(
+                device, "holders", holder))
+        if not holder_realpath:
+            continue
+
+        # run clear holders on all found holders, if it fails, give up
+        (res, _err) = clear_holders(holder_realpath)
+        catcher.caught.extend(_err)
+        if not res:
+            return (False, catcher.caught)
+
+    # detect holder type
+    holder_types = {
+        'bcache': shutdown_bcache,
+        'md': shutdown_mdadm,
+        'dm': shutdown_lvm,
+    }
+    for (type_name, handler) in holder_types.items():
+        if os.path.exists(os.path.join(device, type_name)):
+            with catcher:
+                handler(device)
+
+    # only return true if there are no remaining holders
+    (holders, _err) = get_holders(device)
+    return ((len(holders) == 0 and len(_err) == 0), catcher.caught)
+
+
+def check_clear(device):
+    """
+    Run clear_holders on device.
+
+    If clear_holders fails, dump all exceptions caught by clear_holders to log
+    and raise an OSError
+    """
+    (res, _err) = clear_holders(device)
+    for e in _err:
+        LOG.warn('clear_holders encountered error: {}'.format(e))
+    if not res:
+        raise OSError('could not clear holders for device: {}'.format(e))
