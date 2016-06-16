e9a0379788d55c00ce4ce2e658bd19d1c37e2436
Ryan Harper <ryan.harper@canonical.com>
2016-06-16 12:43:17 -0500

    Fix lint, update unittests with expected output.
    bzr-revno: 392.1.2

--- curtin/net/__init__.py
+++ curtin/net/__init__.py
@@ -447,7 +447,7 @@ def render_interfaces(network_state):
                 if len(subnets) > 1 and index == 0:
                     for i in range(1, len(subnets)):
                         content += "    post-up ifup %s:%s\n" % (iface['name'],
-                                                               i) 
+                                                                 i)
         else:
             # ifenslave docs say to auto the slave devices
             if 'bond-master' in iface:
