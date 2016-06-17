4dab9b125eb49b1388e5983abdcfdf66f07cd677
Christian Ehrhardt <christian.ehrhardt@canonical.com>
2016-06-17 07:25:17 +0000

    extend builtin key of CONFIG_BUILTIN
    It is a single namespace per stage - just "builtin" is too ambiguous.
    
    bzr-revno: 389.7.118

--- curtin/commands/install.py
+++ curtin/commands/install.py
@@ -53,13 +53,13 @@ CONFIG_BUILTIN = {
                'hook', 'late'],
     'early_commands': {},
     'partitioning_commands': {
-        'builtin': ['curtin', 'block-meta', 'simple']},
-    'network_commands': {'builtin': ['curtin', 'net-meta', 'auto']},
-    'extract_commands': {'builtin': ['curtin', 'extract']},
-    'curthooks_commands': {'builtin': ['curtin', 'curthooks']},
-    'hook_commands': {'builtin': ['curtin', 'hook']},
-    'late_commands': {'builtin': []},
-    'apply_net_commands': {'builtin': []},
+        'builtin-block-meta': ['curtin', 'block-meta', 'simple']},
+    'network_commands': {'builtin-net-meta': ['curtin', 'net-meta', 'auto']},
+    'extract_commands': {'builtin-extract': ['curtin', 'extract']},
+    'curthooks_commands': {'builtin-curthooks': ['curtin', 'curthooks']},
+    'hook_commands': {'builtin-hook': ['curtin', 'hook']},
+    'late_commands': {'builtin-late': []},
+    'apply_net_commands': {'builtin-apply-net': []},
     'install': {'log_file': INSTALL_LOG},
 }
 
