d9318f26f0c7f58c3a52a03e179d2b22acd6f5be
Wesley Wiedenmeier <wesley.wiedenmeier@gmail.com>
2016-08-09 16:41:47 -0500

    Updated clear_holders unittests
    bzr-revno: 387.4.108

--- tests/unittests/test_clear_holders.py
+++ tests/unittests/test_clear_holders.py
@@ -7,6 +7,8 @@ import textwrap
 
 
 class TestClearHolders(TestCase):
+    test_blockdev = '/dev/null'
+    test_syspath = '/sys/class/block/null'
     example_holders_trees = [
         [{'device': '/sys/class/block/sda', 'name': 'sda', 'holders':
           [{'device': '/sys/class/block/sda/sda1', 'name': 'sda1',
@@ -69,33 +71,66 @@ class TestClearHolders(TestCase):
     ]
 
     @mock.patch('curtin.block.clear_holders.block')
+    @mock.patch('curtin.block.clear_holders.util')
+    def test_get_dmsetup_uuid(self, mock_util, mock_block):
+        """ensure that clear_holders.get_dmsetup_uuid works as expected"""
+        uuid = "CRYPT-LUKS1-fe335a74374e4649af9776c1699676f8-sdb5_crypt"
+        mock_block.sysfs_to_devpath.return_value = self.test_blockdev
+        mock_util.subp.return_value = (' ' + uuid + '\n', None)
+        res = clear_holders.get_dmsetup_uuid(self.test_syspath)
+        mock_util.subp.assert_called_with(
+            ['dmsetup', 'info', self.test_blockdev, '-C', '-o',
+             'uuid', '--noheadings'], capture=True)
+        self.assertEqual(res, uuid)
+        mock_block.sysfs_to_devpath.assert_called_with(self.test_syspath)
+
+    @mock.patch('curtin.block.clear_holders.block')
     @mock.patch('curtin.block.clear_holders.os')
     def test_get_bcache_using_dev(self, mock_os, mock_block):
         """Ensure that get_bcache_using_dev works"""
         fake_bcache = '/sys/fs/bcache/fake'
         mock_os.path.realpath.return_value = fake_bcache
         mock_os.path.exists.side_effect = lambda x: x == fake_bcache
-        mock_block.sys_block_path.return_value = '/sys/block/vda'
-        bcache_dir = clear_holders.get_bcache_using_dev('/dev/vda')
+        mock_block.sys_block_path.return_value = self.test_blockdev
+        bcache_dir = clear_holders.get_bcache_using_dev(self.test_blockdev)
         self.assertEqual(bcache_dir, fake_bcache)
 
         # test that path is verified before return
         with self.assertRaises(OSError):
-            mock_os.path.realpath.return_value = '/dev/null'
-            clear_holders.get_bcache_using_dev('/dev/vda')
+            mock_os.path.realpath.return_value = self.test_blockdev
+            clear_holders.get_bcache_using_dev(self.test_blockdev)
+
+    @mock.patch('curtin.block.clear_holders.get_dmsetup_uuid')
+    @mock.patch('curtin.block.clear_holders.block')
+    def test_differentiate_lvm_and_crypt(
+            self, mock_block, mock_get_dmsetup_uuid):
+        """test clear_holders.identify_lvm and clear_holders.identify_crypt"""
+        for (kname, dm_uuid, is_lvm, is_crypt) in [
+                ('dm-0', 'LVM-abcdefg', True, False),
+                ('sda', 'LVM-abcdefg', False, False),
+                ('sda', 'CRYPT-abcdefg', False, False),
+                ('dm-0', 'CRYPT-abcdefg', False, True),
+                ('dm-1', 'invalid', False, False)]:
+            mock_block.path_to_kname.return_value = kname
+            mock_get_dmsetup_uuid.return_value = dm_uuid
+            self.assertEqual(
+                is_lvm, clear_holders.identify_lvm(self.test_syspath))
+            self.assertEqual(
+                is_crypt, clear_holders.identify_crypt(self.test_syspath))
+            mock_block.path_to_kname.assert_called_with(self.test_syspath)
+            mock_get_dmsetup_uuid.assert_called_with(self.test_syspath)
 
     @mock.patch('curtin.block.clear_holders.LOG')
     @mock.patch('curtin.block.clear_holders.open')
     @mock.patch('curtin.block.clear_holders.get_bcache_using_dev')
     def test_shutdown_bcache(self, mock_get_bcache, mock_open, mock_log):
         """test clear_holders.shutdown_bcache"""
-        bcache_sys_block = '/sys/block/bcache0'
-        mock_get_bcache.return_value = '/dev/null'
-        clear_holders.shutdown_bcache(bcache_sys_block)
-        mock_get_bcache.assert_called_with(bcache_sys_block)
+        mock_get_bcache.return_value = self.test_blockdev
+        clear_holders.shutdown_bcache(self.test_syspath)
+        mock_get_bcache.assert_called_with(self.test_syspath)
         self.assertTrue(mock_log.debug.called)
         self.assertFalse(mock_log.warn.called)
-        mock_open.assert_called_with('/dev/null/stop', 'w')
+        mock_open.assert_called_with(self.test_blockdev + '/stop', 'w')
 
     @mock.patch('curtin.block.clear_holders.LOG')
     @mock.patch('curtin.block.clear_holders.open')
@@ -112,7 +147,7 @@ class TestClearHolders(TestCase):
             raise OSError('test')
 
         mock_get_bcache.side_effect = raise_os_err
-        clear_holders.shutdown_bcache('/dev/null')
+        clear_holders.shutdown_bcache(self.test_blockdev)
         self.assertFalse(mock_open.called)
         self.assertFalse(mock_log.debug.called)
         self.assertTrue(mock_log.warning.called)
@@ -123,16 +158,15 @@ class TestClearHolders(TestCase):
     @mock.patch('curtin.block.clear_holders.util')
     def test_shutdown_lvm(self, mock_util, mock_lvm, mock_syspath, mock_log):
         """test clear_holders.shutdown_lvm"""
-        device = '/dev/null'
         vg_name = 'volgroup1'
         lv_name = 'lvol1'
-        mock_syspath.return_value = device
+        mock_syspath.return_value = self.test_blockdev
         mock_util.load_file.return_value = '-'.join((vg_name, lv_name))
         mock_lvm.split_lvm_name.return_value = (vg_name, lv_name)
         mock_lvm.get_lvols_in_volgroup.return_value = ['lvol2']
-        clear_holders.shutdown_lvm(device)
-        mock_syspath.assert_called_with(device)
-        mock_util.load_file.assert_called_with(device + '/dm/name')
+        clear_holders.shutdown_lvm(self.test_blockdev)
+        mock_syspath.assert_called_with(self.test_blockdev)
+        mock_util.load_file.assert_called_with(self.test_blockdev + '/dm/name')
         mock_lvm.split_lvm_name.assert_called_with(
             '-'.join((vg_name, lv_name)))
         self.assertTrue(mock_log.debug.called)
@@ -143,20 +177,28 @@ class TestClearHolders(TestCase):
         self.assertEqual(len(mock_util.subp.call_args_list), 1)
         self.assertTrue(mock_lvm.lvm_scan.called)
         mock_lvm.get_lvols_in_volgroup.return_value = []
-        clear_holders.shutdown_lvm(device)
+        clear_holders.shutdown_lvm(self.test_blockdev)
         mock_util.subp.assert_called_with(
             ['vgremove', '--force', '--force', vg_name], rcs=[0, 5])
+
+    @mock.patch('curtin.block.clear_holders.block')
+    @mock.patch('curtin.block.clear_holders.util')
+    def test_shutdown_crypt(self, mock_util, mock_block):
+        """test clear_holders.shutdown_crypt"""
+        mock_block.sysfs_to_devpath.return_value = self.test_blockdev
+        clear_holders.shutdown_crypt(self.test_syspath)
+        mock_block.sysfs_to_devpath.assert_called_with(self.test_syspath)
+        mock_util.subp.assert_called_with(
+            ['cryptsetup', 'remove', self.test_blockdev], capture=True)
 
     @mock.patch('curtin.block.clear_holders.LOG')
     @mock.patch('curtin.block.clear_holders.block')
     def test_shutdown_mdadm(self, mock_block, mock_log):
         """test clear_holders.shutdown_mdadm"""
-        device = '/dev/null'
-        syspath = '/sys/block/null'
-        mock_block.sysfs_to_devpath.return_value = device
-        clear_holders.shutdown_mdadm(syspath)
-        mock_block.mdadm.mdadm_stop.assert_called_with(device)
-        mock_block.mdadm.mdadm_remove.assert_called_with(device)
+        mock_block.sysfs_to_devpath.return_value = self.test_blockdev
+        clear_holders.shutdown_mdadm(self.test_syspath)
+        mock_block.mdadm.mdadm_stop.assert_called_with(self.test_blockdev)
+        mock_block.mdadm.mdadm_remove.assert_called_with(self.test_blockdev)
         self.assertTrue(mock_log.debug.called)
 
     @mock.patch('curtin.block.clear_holders.os')
@@ -166,12 +208,11 @@ class TestClearHolders(TestCase):
     def test_clear_holders_wipe_superblock(self, mock_block, mock_util,
                                            mock_log, mock_os):
         """test clear_holders.wipe_superblock handles errors right"""
-        device = '/dev/null'
-        syspath = '/sys/block/null'
-        mock_block.sysfs_to_devpath.return_value = device
-        clear_holders.wipe_superblock(syspath)
-        mock_block.sysfs_to_devpath.assert_called_with(syspath)
-        mock_block.wipe_volume.assert_called_with(device, mode='superblock')
+        mock_block.sysfs_to_devpath.return_value = self.test_blockdev
+        clear_holders.wipe_superblock(self.test_syspath)
+        mock_block.sysfs_to_devpath.assert_called_with(self.test_syspath)
+        mock_block.wipe_volume.assert_called_with(
+            self.test_blockdev, mode='superblock')
         self.assertFalse(mock_util.is_file_not_found_exc.called)
         self.assertTrue(mock_log.info.called)
 
@@ -182,26 +223,25 @@ class TestClearHolders(TestCase):
         mock_util.is_file_not_found_exc.return_value = True
         mock_util.load_file.return_value = '2'
         mock_os.path.exists.return_value = True
-        clear_holders.wipe_superblock(syspath)
+        clear_holders.wipe_superblock(self.test_syspath)
 
         mock_util.is_file_not_found_exc.return_value = False
         with self.assertRaises(OSError):
-            clear_holders.wipe_superblock(syspath)
+            clear_holders.wipe_superblock(self.test_syspath)
 
     @mock.patch('curtin.block.clear_holders.LOG')
     @mock.patch('curtin.block.clear_holders.block')
     @mock.patch('curtin.block.clear_holders.os')
     def test_get_holders(self, mock_os, mock_block, mock_log):
         """test clear_holders.get_holders"""
-        sysfs_path = '/sys/block/null'
-        device = '/dev/null'
-        mock_block.sys_block_path.return_value = sysfs_path
+        mock_block.sys_block_path.return_value = self.test_syspath
         mock_os.path.join.side_effect = os.path.join
-        clear_holders.get_holders(device)
-        mock_block.sys_block_path.assert_called_with(device)
-        mock_os.path.join.assert_called_with(sysfs_path, 'holders')
+        clear_holders.get_holders(self.test_blockdev)
+        mock_block.sys_block_path.assert_called_with(self.test_blockdev)
+        mock_os.path.join.assert_called_with(self.test_syspath, 'holders')
         self.assertTrue(mock_log.debug.called)
-        mock_os.listdir.assert_called_with(os.path.join(sysfs_path, 'holders'))
+        mock_os.listdir.assert_called_with(
+            os.path.join(self.test_syspath, 'holders'))
 
     def test_plan_shutdown_holders_trees(self):
         """
