76d57c067fb50e3af0c57e17a8e7845a507f4a03
Wesley Wiedenmeier <wesley.wiedenmeier@gmail.com>
2016-10-01 20:38:30 -0500

    Apply BOOTIF= kernel cmdline arg for all centos vmtest classes
    bzr-revno: 426.2.21

--- tests/vmtests/test_centos_basic.py
+++ tests/vmtests/test_centos_basic.py
@@ -5,9 +5,8 @@ from .releases import centos_base_vm_classes as relbase
 # FIXME: should eventually be integrated with the real TestBasic
 class CentosTestBasicAbs(VMBaseClass):
     __test__ = False
-    # FIXME: get this working with a non-custom test yaml
-    #       (and put it in examples/)
     conf_file = "examples/tests/centos_basic.yaml"
+    extra_kern_args = "BOOTIF=eth0-52:54:00:12:34:00"
     collect_scripts = []
 
     def test_dname(self):
@@ -18,7 +17,6 @@ class CentosTestBasicAbs(VMBaseClass):
 class Centos70FromXenialTestBasic(relbase.centos70fromxenial,
                                   CentosTestBasicAbs):
     __test__ = True
-    extra_kern_args = "BOOTIF=eth0-52:54:00:12:34:00"
 
 
 class Centos66FromXenialTestBasic(relbase.centos66fromxenial,
