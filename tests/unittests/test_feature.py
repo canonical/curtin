c2d0908baf20b9a0e42b5b5247f527e013f3392c
Andres Rodriguez <andreserl@ubuntu.com>
2017-03-21 12:41:33 -0400

    Add STORAGE_CONFIG_V1_DD to represent the ability to configure storage for DD images
    bzr-revno: 477.1.1

--- tests/unittests/test_feature.py
+++ tests/unittests/test_feature.py
@@ -10,5 +10,8 @@ class TestExportsFeatures(TestCase):
     def test_has_network_v1(self):
         self.assertIn('NETWORK_CONFIG_V1', curtin.FEATURES)
 
+    def test_has_network_v1_dd(self):
+        self.assertIn('NETWORK_CONFIG_V1_DD', curtin.FEATURES)
+
     def test_has_reporting_events_webhook(self):
         self.assertIn('REPORTING_EVENTS_WEBHOOK', curtin.FEATURES)
