d9f25242d430bcd8586286bccd255ae49e212c45
Ryan Harper <ryan.harper@canonical.com>
2017-05-22 15:10:23 -0500

    mock fuser_mount instead of subp
    bzr-revno: 498.1.26

--- tests/unittests/test_block.py
+++ tests/unittests/test_block.py
@@ -277,7 +277,7 @@ class TestWipeFile(TestCase):
         mock_os_fdopen.assert_called_with(mock_fd, 'rb+')
         self.assertEqual([], mock_os_close.call_args_list)
 
-    @mock.patch('curtin.util.subp')
+    @mock.patch('curtin.util.fuser_mount')
     @mock.patch('os.close')
     @mock.patch('curtin.util.list_device_mounts')
     @mock.patch('curtin.block.get_holders')
@@ -286,14 +286,14 @@ class TestWipeFile(TestCase):
                                                     mock_holders,
                                                     mock_list_mounts,
                                                     mock_os_close,
-                                                    mock_util_subp):
+                                                    mock_util_fuser):
         flen = 1024
         myfile = self.tfile("my_exclusive_file")
         util.write_file(myfile, flen * b'\1', omode="wb")
         mock_os_open.side_effect = OSError("NO_O_EXCL")
         mock_holders.return_value = ['md1']
         mock_list_mounts.return_value = []
-        mock_util_subp.return_value = ("", "")
+        mock_util_fuser.return_value = {}
 
         with self.assertRaises(OSError):
             with block.exclusive_open(myfile) as fp:
