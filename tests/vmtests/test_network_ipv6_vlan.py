5950d761e3ea322eea1c3ce35cc3bae7a52029a9
Ryan Harper <ryan.harper@canonical.com>
2017-06-02 11:43:49 -0500

    Add centos to network_ipv6_vlan vmtest
    bzr-revno: 504.2.13

--- tests/vmtests/test_network_ipv6_vlan.py
+++ tests/vmtests/test_network_ipv6_vlan.py
@@ -1,8 +1,14 @@
 from .releases import base_vm_classes as relbase
-from .test_network_vlan import TestNetworkVlanAbs
+from .releases import centos_base_vm_classes as centos_relbase
+from .test_network_vlan import (TestNetworkVlanAbs,
+                                CentosTestNetworkVlanAbs)
 
 
 class TestNetworkIPV6VlanAbs(TestNetworkVlanAbs):
+    conf_file = "examples/tests/vlan_network_ipv6.yaml"
+
+
+class CentosTestNetworkIPV6VlanAbs(CentosTestNetworkVlanAbs):
     conf_file = "examples/tests/vlan_network_ipv6.yaml"
 
 
@@ -44,4 +50,14 @@ class ZestyTestNetworkIPV6Vlan(relbase.zesty, TestNetworkIPV6VlanAbs):
 
 
 class ArtfulTestNetworkIPV6Vlan(relbase.artful, TestNetworkIPV6VlanAbs):
+    __test__ = True
+
+
+class Centos66TestNetworkIPV6Vlan(centos_relbase.centos66fromxenial,
+                                  CentosTestNetworkIPV6VlanAbs):
+    __test__ = True
+
+
+class Centos70TestNetworkIPV6Vlan(centos_relbase.centos70fromxenial,
+                                  CentosTestNetworkIPV6VlanAbs):
     __test__ = True
