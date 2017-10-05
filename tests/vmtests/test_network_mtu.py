f2b7eaa90f0771b5fc4f2029fc492169f6a6792a
Scott Moser <smoser@ubuntu.com>
2017-10-05 15:43:08 -0400

    drop my failed attempt at short. thanks chad.
    bzr-revno: 529.4.10

--- tests/vmtests/test_network_mtu.py
+++ tests/vmtests/test_network_mtu.py
@@ -135,13 +135,24 @@ class CentosTestNetworkMtuAbs(TestNetworkMtuAbs):
         pass
 
     @unittest.skip("Sysconfig does not support mixed v4/v6 MTU: LP:#1706973")
-    def _nomixmtu(self):
+    def test_ip_output(self):
         pass
 
-    test_ipv6_mtu_smaller_than_ipv4_v6_iface_first = _nomixmtu
-    test_ipv6_mtu_smaller_than_ipv4_non_default = _nomixmtu
-    test_ipv6_mtu_higher_than_default_no_ipv4_iface_up = _nomixmtu
-    test_ipv6_mtu_higher_than_default_no_ipv4_iface_v6_iface_first = _nomixmtu
+    @unittest.skip("Sysconfig does not support mixed v4/v6 MTU: LP:#1706973")
+    def test_ipv6_mtu_smaller_than_ipv4_v6_iface_first(self):
+        pass
+
+    @unittest.skip("Sysconfig does not support mixed v4/v6 MTU: LP:#1706973")
+    def test_ipv6_mtu_smaller_than_ipv4_non_default(self):
+        pass
+
+    @unittest.skip("Sysconfig does not support mixed v4/v6 MTU: LP:#1706973")
+    def test_ipv6_mtu_higher_than_default_no_ipv4_iface_up(self):
+        pass
+
+    @unittest.skip("Sysconfig does not support mixed v4/v6 MTU: LP:#1706973")
+    def test_ipv6_mtu_higher_than_default_no_ipv4_iface_v6_iface_first(self):
+        pass
 
 
 class PreciseHWETTestNetworkMtu(relbase.precise_hwe_t, TestNetworkMtuAbs):
