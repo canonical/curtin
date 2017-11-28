acc2d67fe397afae3bdf7b833722854da306ef0c
Ryan Harper <ryan.harper@canonical.com>
2017-11-28 15:08:46 -0600

    Refactor mkfs parameter generation
    Allow some mkfs parameters to accept a param formatter.  Some options like
    'force' and 'quiet' are flag-only options which do not accept a param.
    Those values don't need a tuple for their value, but just the flag string.
    
    bzr-revno: 541.4.7

--- tests/unittests/test_block_mkfs.py
+++ tests/unittests/test_block_mkfs.py
@@ -67,9 +67,10 @@ class TestBlockMkfs(CiTestCase):
         self._run_mkfs_with_config(conf, "mkfs.btrfs", expected_flags)
 
     def test_mkfs_xfs(self):
+        """ mkfs.xfs passes uuid parameter """
         conf = self._get_config("xfs")
         expected_flags = ['-f', ['-L', 'format1'],
-                          ['-m', 'uuid=%s' % self.test_uuid]]
+                          '-m uuid=%s' % self.test_uuid]
         self._run_mkfs_with_config(conf, "mkfs.xfs", expected_flags)
 
     def test_mkfs_btrfs_on_precise(self):
