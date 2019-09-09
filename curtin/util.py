5f132478044b60650857750dda16307f207a05de
Ryan Harper <ryan.harper@canonical.com>
2019-09-09 18:42:38 +0000

    util: add support for 'tbz', 'txz' tar format types to sanitize_source
    Curtin already supports tar.bz[2] and tar.xz via the helpers/smtar
    tool but is missing the short-hand used in santize_sources to convert
    a source URI into a source dictionary.  This branch adds the two types
    and adds unittest.
    
    LP: #1843266

--- curtin/util.py
+++ curtin/util.py
@@ -906,8 +906,9 @@ def sanitize_source(source):
     if type(source) is dict:
         # already sanitized?
         return source
-    supported = ['tgz', 'dd-tgz', 'dd-tbz', 'dd-txz', 'dd-tar', 'dd-bz2',
-                 'dd-gz', 'dd-xz', 'dd-raw', 'fsimage', 'fsimage-layered']
+    supported = ['tgz', 'dd-tgz', 'tbz', 'dd-tbz', 'txz', 'dd-txz', 'dd-tar',
+                 'dd-bz2', 'dd-gz', 'dd-xz', 'dd-raw', 'fsimage',
+                 'fsimage-layered']
     deftype = 'tgz'
     for i in supported:
         prefix = i + ":"
