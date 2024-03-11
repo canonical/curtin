2419695d23aead77cab1360c2871fde0aabe02f5
Olivier Gayot <olivier.gayot@canonical.com>
2024-03-11 08:25:18 +0000

    apt-config: enable deb822 sources from 24.04 onwards
    This reverts commit a7640fdcac396f9f09044dc7ca7553043ce4231c and applies
    further modifications to enable deb822 sources starting from 24.04 and
    not 23.10.
    
    LP: #2056308
    
    Signed-off-by: Olivier Gayot <olivier.gayot@canonical.com>
    (cherry picked from commit 3d0994a3bfb47318bd8b4c0aa5e345e198eabe77)

--- curtin/commands/apt_config.py
+++ curtin/commands/apt_config.py
@@ -75,15 +75,8 @@ def want_deb822(target=None):
     False otherwise.
 
     This is determined by the version of Ubuntu: deb822 is the default starting
-    with Ubuntu 23.10 (Mantic Minotaur).
+    with Ubuntu 24.04 (Noble Numbat).
     """
-
-    """
-    FIXME: The deb822-by-default goal was delayed in 23.10.
-    Re-enable/update this code (and the test case in
-    tests/unittests/test_apt_source.py) when we are ready for deb822 by
-    default.
-
     os_release = distro.os_release(target)
 
     if os_release.get('ID') != 'ubuntu':
@@ -97,10 +90,7 @@ def want_deb822(target=None):
     if len(version) != 2:
         return False
 
-    return version[0] > 23 or (version[0] >= 23 and version[1] >= 10)
-    """
-
-    return False
+    return version[0] >= 24
 
 
 class Deb822SourceFormatException(Exception):
